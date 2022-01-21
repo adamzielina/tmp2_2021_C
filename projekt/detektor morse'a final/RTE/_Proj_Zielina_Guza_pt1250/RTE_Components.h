bef88ea1985f4860dd05674f2878f89acc846a4b
adamzielina <56132235+adamzielina@users.noreply.github.com>
2022-01-21 23:52:21 +0100

    Add files via upload

---
+++ projekt/detektor morse'a final/RTE/_Proj_Zielina_Guza_pt1250/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Proj_Zielina_Guza_pt1250' 
+ * Target:  'Proj_Zielina_Guza_pt1250' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "MKL05Z4.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
