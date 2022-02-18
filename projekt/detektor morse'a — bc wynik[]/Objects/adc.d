7bdf0ff8600d72f9bbaf50afa1c3e97be34dae11
adamzielina <56132235+adamzielina@users.noreply.github.com>
2022-02-19 00:50:02 +0100

    Delete projekt/detektor morse'a — bc wynik[] directory

--- projekt/detektor morse'a — bc wynik[]/Objects/adc.d
+++
@@ -1,9 +0,0 @@
-.\objects\adc.o: ADC.c
-.\objects\adc.o: ADC.h
-.\objects\adc.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
-.\objects\adc.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\adc.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\adc.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\adc.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\adc.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\adc.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
--- projekt/detektor morse'a — bc wynik[]/Objects/pit.d
+++
@@ -1,9 +0,0 @@
-.\objects\pit.o: pit.c
-.\objects\pit.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
-.\objects\pit.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\pit.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\pit.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\pit.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\pit.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\pit.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
-.\objects\pit.o: pit.h
