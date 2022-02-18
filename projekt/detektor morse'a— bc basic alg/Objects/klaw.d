2ee8ac77bbebe5b5fe8b4283634d881abd815514
adamzielina <56132235+adamzielina@users.noreply.github.com>
2022-02-19 00:50:14 +0100

    Delete projekt/detektor morse'a— bc basic alg directory

--- projekt/detektor morse'a— bc basic alg/Objects/klaw.d
+++
@@ -1,9 +0,0 @@
-.\objects\klaw.o: klaw.c
-.\objects\klaw.o: klaw.h
-.\objects\klaw.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL05Z4.h
-.\objects\klaw.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
-.\objects\klaw.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\klaw.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
-.\objects\klaw.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
-.\objects\klaw.o: D:\keil pack\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
-.\objects\klaw.o: D:\keil pack\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\system_MKL05Z4.h
